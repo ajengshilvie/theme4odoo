7c2bd1fc20b9040cc07ae046097c7f3fbdaa2a8a
ajeng <ajeng@ngodoo.com>
2016-09-26 11:52:48 +0700

    add module

---
+++ asn_odoo_theme/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name': 'ASN Odoo 9 Theme',
+    'version': '1.0',
+    'author': 'ASN',
+    'description': '''
+        A custom module to change default odoo menu color
+    ''',
+    'category': 'Themes/ASN',
+    'depends': [
+        'base',
+    ],
+    'data': [
+        'views/custom_view.xml',
+    ],
+    'css': ['static/src/css/styles.css'],
+    'auto_install': False,
+    'installable': True,
+}
