f65cf66f23cf174b40f46afcb1cb1b13dcdfb283
ajeng <ajeng@ngodoo.com>
2016-11-01 19:02:50 +0700

    Initial Theme

---
+++ shamrock_backend_theme/__openerp__.py
@@ -0,0 +1,18 @@
+{
+    'name' : 'Shamrock Odoo 10 Backend Theme',
+    'version' : '1.0',
+    'author' : 'Ajeng Shilvie N',
+    'category' : 'Backend Theme',
+    'summary': 'Its Shamrock !! Inspired by OpenWorx and Farrel Rafi. This backEnd theme may not like the enterprise version. But have a taste ! ',
+    'description': """
+        This backEnd theme may not like the enterprise version. But have a taste ! 
+    """,
+    'images':[
+        'images/sales.png'
+    ],
+    'depends' : ['base','web'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
