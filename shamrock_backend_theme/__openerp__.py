5c74ec5a0808532f3f979a9b9a878be21d3629d6
ajeng <ajeng@ngodoo.com>
2016-11-01 19:10:39 +0700

    add main screenshot

--- shamrock_backend_theme/__openerp__.py
+++ shamrock_backend_theme/__openerp__.py
@@ -2,13 +2,13 @@
     'name' : 'Shamrock Odoo 10 Backend Theme',
     'version' : '1.0',
     'author' : 'Ajeng Shilvie N',
-    'category' : 'Backend Theme',
+    'category' : 'Themes/ASN',
     'summary': 'Its Shamrock !! Inspired by OpenWorx and Farrel Rafi. This backEnd theme may not like the enterprise version. But have a taste ! ',
     'description': """
         This backEnd theme may not like the enterprise version. But have a taste ! 
     """,
     'images':[
-        'images/sales.png'
+            'static/description/main_screenshot.jpg',
     ],
     'depends' : ['base','web'],
     'data':[
