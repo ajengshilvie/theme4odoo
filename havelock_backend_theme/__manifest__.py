76f7816b2b27e86961dbc58acda5497c975f2979
ajeng <ajeng@ngodoo.com>
2017-01-06 10:51:08 +0700

    push havelock theme

---
+++ havelock_backend_theme/__manifest__.py
@@ -0,0 +1,19 @@
+{
+    'name' : 'Havelock Backend Theme',
+    'version' : '10.0',
+    'author' : 'Ajeng Shilvie N',
+    'category' : 'Themes/ASN',
+    'website' : '',
+    'summary': 'Its Havelock Blue !! Inspired by OpenWorx. This theme created for Odoo Community Edition ! ',
+    'description': """
+        This backEnd theme may not like the enterprise version. But have a taste !
+    """,
+    'images':[
+            'static/description/main_screenshot.png',
+    ],
+    'depends' : ['base','web'],
+    'data':[
+        'views.xml',
+        ],
+    'installable': True
+}
