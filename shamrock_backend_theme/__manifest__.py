94016266929001267bdabf9b24d8d4946489555f
ajeng <ajeng039e@gmail.com>
2017-03-31 10:48:44 +0700

    change category __openerp__ to __manifest__

--- shamrock_backend_theme/__manifest__.py
+++ shamrock_backend_theme/__manifest__.py
@@ -2,7 +2,7 @@
     'name' : 'Shamrock Backend Theme',
     'version' : '1.0',
     'author' : 'Ajeng Shilvie N',
-    'category' : 'Themes/ASN',
+    'category' : 'Themes/Backend',
     'summary': 'Its Shamrock !! Inspired by OpenWorx and Farrel Rafi. This backEnd theme may not like the enterprise version. But have a taste ! ',
     'description': """
         This backEnd theme may not like the enterprise version. But have a taste ! 
